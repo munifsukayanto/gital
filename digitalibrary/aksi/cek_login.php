e9645e15f17273595cd7229c6ecd3fc3c3be82b5
munifsukayanto <157446520+munifsukayanto@users.noreply.github.com>
2024-01-30 12:54:16 +0700

    Add files via upload

---
+++ digitalibrary/aksi/cek_login.php
@@ -0,0 +1,59 @@
+<?php
+// mengaktifkan sesion pada php
+session_start();
+
+// menghubungkan php dengan koneksi database
+include '../koneksi/koneksi.php';
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+// menyelesaikan data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi, "SELECT * from user where username='$username' and password='$password'");
+
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+
+    $data = mysqli_fetch_assoc($login);
+
+    // cek jika user login dan username
+    if($data['level']=="admin"){
+
+        // buat session login dan username
+        $_SESSION['username'] = $username;
+        $_SESSION['level'] = "admin";
+        // alihkan ke halaman dashboard admin
+        header("location:../admin/admin.php");
+
+        // cek jika user login sebagai petugas
+        }else if($data['level']=="petugas"){
+        // buat session login dan username
+        $_SESSION['username'] = $username;
+        $_SESSION['level'] = "petugas";
+        // alihkan ke halaman dashboard admin
+        header("location:../petugas/petugas.php");
+
+        // cek jika user login sebagai peminjam
+        }else if($data['level']=="peminjam"){
+        // buat session login dan username
+        $_SESSION['username'] = $username;
+        $_SESSION['level'] = "peminjam";
+        // alihkan ke halaman dashboard admin
+        header("location:../peminjam/peminjaman.php");
+
+        }else{
+
+            // alihkan ke halaman login kembali
+            header("location:../login.php?pesan=gagal");
+        }
+
+        }else{
+            
+            header("location:../admin/admin.php?pesan=gagal");
+        }
+
+        ?>
